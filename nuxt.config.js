fe972796d0aea9dd31515d591a11b08a5957110e
aproskam <ap@rskm.nl>
2021-06-07 22:15:27 +0200

    adjust nuxt config, add temp. route solution

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import axios from 'axios'
-
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -9,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'rskm.nl',
+    title: 'ArnoPaul Roskam — Front-End Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -41,44 +39,11 @@ export default {
     storyblokCacheProvider: process.env.STORYBLOK_CACHE_PROVIDER,
   },
 
-  privateRuntimeConfig: {
-    storyblokAccessToken: process.env.STORYBLOK_ACCESS_TOKEN,
-    storyblokCacheProvider: process.env.STORYBLOK_CACHE_PROVIDER,
-  },
-
   // Using Links API (100 entries - no pagination in the example)
   generate: {
-    routes(callback) {
-      const token = process.env.STORYBLOK_ACCESS_TOKEN
-      const version = 'published'
-      let cacheVersion = 0
-      const toIgnore = ['/', 'en/settings']
-
-      // other routes that are not in Storyblok with their slug.
-      const routes = ['/'] // adds / directly
-      // Load space and receive latest cache version key to improve performance
-      axios
-        .get(`https://api.storyblok.com/v1/cdn/spaces/me?token=${token}`)
-        .then((spaceRes) => {
-          // timestamp of latest publish
-          cacheVersion = spaceRes.data.space.version
-
-          // Call for all Links using the Links API: https://www.storyblok.com/docs/Delivery-Api/Links
-          axios
-            .get(
-              `https://api.storyblok.com/v1/cdn/links?token=${token}&version=${version}&cv=${cacheVersion}&per_page=100`
-            )
-            .then((res) => {
-              Object.keys(res.data.links).forEach((key) => {
-                if (!toIgnore.includes(res.data.links[key].slug)) {
-                  routes.push('/' + res.data.links[key].slug)
-                }
-              })
-              callback(null, routes)
-            })
-        })
-    },
+    routes: ['/work/cakefilm-case', '/work/portfolio-website'],
   },
+  // TODO: Make routes generation dynamic
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
