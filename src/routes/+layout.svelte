0752b8b8fbd3716493ad7ae4d87049914a9c2167
aproskam <ap@rskm.nl>
2022-09-26 20:22:28 +0200

    configure fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
   import Footer from '$lib/footer/Footer.svelte';
+  import '../lib/css/fonts.css';
+  import '../lib/css/typography.css';
 	import '../app.css';
 </script>
 
