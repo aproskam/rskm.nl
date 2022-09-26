61eaaecded17006bc1bbb0bacb14b2faed3f4ac4
aproskam <ap@rskm.nl>
2022-09-26 20:03:07 +0200

    add workOverview component

---
+++ src/lib/WorkOverview.svelte
@@ -0,0 +1,11 @@
+<script>
+  import WorkCard from "./WorkCard.svelte";
+
+  export let cases;
+</script>
+
+<section>
+  {#each cases as work}
+    <WorkCard work={work.fields} />
+  {/each}
+</section>
