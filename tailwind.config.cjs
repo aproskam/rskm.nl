173b774dd192c4c93174623c8757d1c7c5ee280e
aproskam <ap@rskm.nl>
2022-09-26 20:22:45 +0200

    add tailwind aspect-ratio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    fontFamily: {
+      sans: ['Inter', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      height: {
+        'screen-2/4': '50vh',
+      },
+    },
   },
-  plugins: []
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
