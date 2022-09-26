0bb103ea2d274b27fca6905eecc3c709674611ad
aproskam <ap@rskm.nl>
2022-09-26 20:03:35 +0200

    add intro component

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+
+  export let fields;
+
+  const introText = documentToHtmlString(fields.introText);
+</script>
+
+<section>
+  <div>
+    <img width="200" height="300" src={fields.introImage.fields.file.url} alt={fields.introImage.fields.title} />
+  </div>
+
+  <div class="style--nested-copy">
+    {@html introText}
+  </div>
+</section>
