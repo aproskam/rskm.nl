5bb343fc8ba54a6b70f05a8748d87f291a2c04e9
aproskam <ap@rskm.nl>
2022-10-09 17:13:40 +0200

    add homepage template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,18 @@
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+<script>
+	import Intro from '$lib/Intro.svelte';
+  import WorkOverview from '$lib/work/WorkOverview.svelte';
+
+	export let data;
+
+  $: console.log("HP: ", data);
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>{data.fields.seoTitle}</title>
+	<meta name="description" content="" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+  <Intro fields={data.fields} />
+  <WorkOverview cases={data.workData} />
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
