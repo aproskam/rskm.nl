d66a66dd33f64542b6efbc8894815886c542b900
aproskam <ap@rskm.nl>
2022-10-09 21:49:40 +0200

    add Intro component

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -1,13 +1,42 @@
 <script>
+  import ImaginePicture from "./Imagine/ImaginePicture.svelte";
   import NestedRichText from "./NestedRichText.svelte";
 
   export let fields;
+
+  const imageData = {
+    ...fields.introImage.fields,
+    'id': fields.introImage.sys.id
+  }
 </script>
 
-<section>
-  <div>
-    <img width="200" height="300" src={fields.introImage.fields.file.url} alt={fields.introImage.fields.title} />
+<section class="
+flex flex-col
+lg:flex-row
+justify-center
+h-screen
+w-screen
+items-center
+">
+  <div class="lg:block w-full lg:w-1/3 lg:h-full relative mb-8 lg:mb-0">
+    <ImaginePicture 
+      imageData={imageData} 
+      transforms={['100x100', '400x400', '800x800', '1200x1200']} 
+      imgLoading={'eager'}
+      customClass={"lg:hidden rounded-full max-w-none mx-auto object-cover"}
+    />
+
+    <ImaginePicture 
+      imageData={imageData} 
+      transforms={['100x0', '400x0', '800x0', '1200x0']} 
+      imgLoading={'eager'}
+      customClass={"hidden lg:block lg:rounded-none lg:absolute lg:inset-0 lg:w-full lg:h-full max-w-none object-cover"}
+      height={imageData.file.details.image.height};
+      width={imageData.file.details.image.width};
+    />
   </div>
 
-  <NestedRichText richTextData={fields.introText} />
+  <div class="lg:w-2/3 p-4 lg:p-16 style--nested-copy">
+    <NestedRichText richTextData={fields.introText} />
+  </div>
 </section>
