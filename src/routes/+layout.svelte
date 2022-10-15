bdde8051c4460df5c281886b5d82bf2341753a50
aproskam <ap@rskm.nl>
2022-10-15 15:46:36 +0200

    add page loader indicator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,18 @@
 <script>
+  import { navigating } from '$app/stores';
 	import Header from '$lib/header/Header.svelte';
   import Footer from '$lib/footer/Footer.svelte';
+  import PreloadingIndicator from '$lib/PreloadingIndicator.svelte';
   import '../lib/css/fonts.css';
 	import '../app.css';
   import '../lib/css/typography.css';
 </script>
 
 <div class="relative font-sans">
+  {#if $navigating}
+    <PreloadingIndicator />
+  {/if}
+
   <Header />
   
   <main class="-mt-14">
