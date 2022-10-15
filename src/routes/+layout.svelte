9fc84bb250f80aadac20a5df9ba7a5f3752ae6cd
aproskam <ap@rskm.nl>
2022-10-15 18:36:13 +0200

    fix class typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
   import '../lib/css/typography.css';
 </script>
 
-<div class="relative font-sans night-blue">
+<div class="relative font-sans text-night-blue">
   {#if $navigating}
     <PreloadingIndicator />
   {/if}
